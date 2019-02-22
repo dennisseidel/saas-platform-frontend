7a02aa761f8e5e52d32e4aa5260ac47f82633136
Dennis Seidel <den.seidel@gmail.com>
2019-02-22 15:30:23 +0100

    add landing page

---
+++ frontend/src/pages/Home/data.source.js
@@ -0,0 +1,368 @@
+import React from 'react';
+export const Banner00DataSource = {
+  wrapper: { className: 'banner0' },
+  textWrapper: { className: 'banner0-text-wrapper' },
+  title: {
+    className: 'banner0-title',
+    children: 'https://i.imgur.com/mIA0ssx.png',
+  },
+  content: {
+    className: 'banner0-content',
+    children: (
+      <span>
+        <p>
+          Bootstrap your digital health business with compliant cloud services
+        </p>
+      </span>
+    ),
+  },
+  button: { className: 'banner0-button', children: 'Learn More' },
+};
+export const Content00DataSource = {
+  wrapper: { className: 'home-page-wrapper content0-wrapper' },
+  page: { className: 'home-page content0' },
+  OverPack: { playScale: 0.3, className: '' },
+  titleWrapper: {
+    className: 'title-wrapper',
+    children: [
+      {
+        name: 'title',
+        children: (
+          <span>
+            <p>Products and Services</p>
+          </span>
+        ),
+      },
+    ],
+  },
+  block: {
+    className: 'block-wrapper',
+    children: [
+      {
+        name: 'block0',
+        className: 'block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'icon',
+            children:
+              'https://zos.alipayobjects.com/rmsportal/WBnVOjtIlGWbzyQivuyq.png',
+          },
+          title: {
+            className: 'content0-title',
+            children: (
+              <span>
+                <p>Digital Medical Record aaS</p>
+              </span>
+            ),
+          },
+          content: {
+            children: (
+              <span>
+                <p>
+                  Add a digital medical record to your product through simple
+                  APIs.
+                </p>
+              </span>
+            ),
+          },
+        },
+      },
+      {
+        name: 'block1',
+        className: 'block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'icon',
+            children:
+              'https://zos.alipayobjects.com/rmsportal/YPMsLQuCEXtuEkmXTTdk.png',
+          },
+          title: {
+            className: 'content0-title',
+            children: (
+              <span>
+                <p>Medical Machine Learning aaS</p>
+              </span>
+            ),
+          },
+          content: {
+            children: (
+              <span>
+                <p>
+                  Train or use pre-trained machine learning models through
+                  simple APIs.
+                </p>
+              </span>
+            ),
+          },
+        },
+      },
+      {
+        name: 'block2',
+        className: 'block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'icon',
+            children:
+              'https://zos.alipayobjects.com/rmsportal/EkXWVvAaFJKCzhMmQYiX.png',
+          },
+          title: {
+            className: 'content0-title',
+            children: (
+              <span>
+                <p>Scheduling and digital assistant aaS&nbsp;</p>
+              </span>
+            ),
+          },
+          content: {
+            children: (
+              <span>
+                <p>
+                  Add an assistant (supporting text and voice) to your product
+                  through simple APIs.
+                </p>
+              </span>
+            ),
+          },
+        },
+      },
+    ],
+  },
+};
+export const Content30DataSource = {
+  wrapper: { className: 'home-page-wrapper content3-wrapper' },
+  page: { className: 'home-page content3' },
+  OverPack: { playScale: 0.3 },
+  titleWrapper: {
+    className: 'title-wrapper',
+    children: [
+      {
+        name: 'title',
+        children: (
+          <span>
+            <p>The Germany Health Cloud Platform provides compliant services</p>
+          </span>
+        ),
+        className: 'title-h1',
+      },
+      {
+        name: 'content',
+        className: 'title-content',
+        children: (
+          <span>
+            <p>Based on AWS's powerful basic resources</p>
+          </span>
+        ),
+      },
+    ],
+  },
+  block: {
+    className: 'content3-block-wrapper',
+    children: [
+      {
+        name: 'block0',
+        className: 'content3-block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'content3-icon',
+            children:
+              'https://zos.alipayobjects.com/rmsportal/ScHBSdwpTkAHZkJ.png',
+          },
+          textWrapper: { className: 'content3-text' },
+          title: {
+            className: 'content3-title',
+            children: (
+              <span>
+                <p>Data Protection Compliant</p>
+              </span>
+            ),
+          },
+          content: {
+            className: 'content3-content',
+            children: (
+              <span>
+                <p>
+                  All data is end to end encrypted and uses GDPR, §203 and HIPAA
+                  compliant providers.
+                </p>
+              </span>
+            ),
+          },
+        },
+      },
+      {
+        name: 'block1',
+        className: 'content3-block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'content3-icon',
+            children:
+              'https://zos.alipayobjects.com/rmsportal/NKBELAOuuKbofDD.png',
+          },
+          textWrapper: { className: 'content3-text' },
+          title: {
+            className: 'content3-title',
+            children: (
+              <span>
+                <p>Vendor Independent</p>
+              </span>
+            ),
+          },
+          content: {
+            className: 'content3-content',
+            children: (
+              <span>
+                <p>
+                  Supporting open standard like&nbsp;DICOM &amp; HL7 you can
+                  communicate with systems from other vendors.
+                </p>
+              </span>
+            ),
+          },
+        },
+      },
+      {
+        name: 'block2',
+        className: 'content3-block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'content3-icon',
+            children:
+              'https://zos.alipayobjects.com/rmsportal/xMSBjgxBhKfyMWX.png',
+          },
+          textWrapper: { className: 'content3-text' },
+          title: {
+            className: 'content3-title',
+            children: (
+              <span>
+                <p>Cloud Developer Experience</p>
+              </span>
+            ),
+          },
+          content: {
+            className: 'content3-content',
+            children: (
+              <span>
+                <p>
+                  Products are self service and well documented allowing
+                  developers to get&nbsp; productive within minuites.
+                </p>
+              </span>
+            ),
+          },
+        },
+      },
+      {
+        name: 'block3',
+        className: 'content3-block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'content3-icon',
+            children:
+              'https://zos.alipayobjects.com/rmsportal/MNdlBNhmDBLuzqp.png',
+          },
+          textWrapper: { className: 'content3-text' },
+          title: {
+            className: 'content3-title',
+            children: (
+              <span>
+                <p>Cloud Scalability</p>
+              </span>
+            ),
+          },
+          content: {
+            className: 'content3-content',
+            children: (
+              <span>
+                <p>
+                  Using the clouds the offerings allow you to scale instant with
+                  your customers demand without quality&nbsp;impairments.&nbsp;
+                </p>
+              </span>
+            ),
+          },
+        },
+      },
+      {
+        name: 'block4',
+        className: 'content3-block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'content3-icon',
+            children:
+              'https://zos.alipayobjects.com/rmsportal/UsUmoBRyLvkIQeO.png',
+          },
+          textWrapper: { className: 'content3-text' },
+          title: {
+            className: 'content3-title',
+            children: (
+              <span>
+                <p>Reduced Mainetance</p>
+              </span>
+            ),
+          },
+          content: {
+            className: 'content3-content',
+            children: (
+              <span>
+                <p>
+                  Offerings are maintained by the platform making on-site
+                  maintenance redundant, but available if required!&nbsp;
+                </p>
+              </span>
+            ),
+          },
+        },
+      },
+      {
+        name: 'block5',
+        className: 'content3-block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'content3-icon',
+            children:
+              'https://zos.alipayobjects.com/rmsportal/ipwaQLBLflRfUrg.png',
+          },
+          textWrapper: { className: 'content3-text' },
+          title: {
+            className: 'content3-title',
+            children: (
+              <span>
+                <p>Offline Resilience</p>
+              </span>
+            ),
+          },
+          content: {
+            className: 'content3-content',
+            children: (
+              <span>
+                <p>
+                  The Health Cloud Platform SDK offers the resilience
+                  functionality in case of unavailability - keeping you
+                  productive.
+                </p>
+              </span>
+            ),
+          },
+        },
+      },
+    ],
+  },
+};
