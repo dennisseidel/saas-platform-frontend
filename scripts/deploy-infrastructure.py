d97e8abd90b3d473ad1da8211b809400c3d584f6
Dennis Seidel <den.seidel@gmail.com>
2019-06-21 19:11:54 +0200

    chore: check if test user exist before creating

--- scripts/deploy-infrastructure.py
+++ scripts/deploy-infrastructure.py
@@ -2,9 +2,20 @@
 import os
 # import shutil
 import boto3
-# import botocore
 import argparse
 import json
+
+
+def user_missing(user_pool_id, username):
+    try:
+        client.admin_get_user(
+            UserPoolId=user_pool_id,
+            Username=username
+        )
+        return True
+    except:
+        return False
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -34,18 +45,20 @@ if args.component == "infrastructure":
             USER_POOL_ID = config['user_pool_id']['value']
             TEST_USER = os.environ["TEST_USER"]
             TEST_USER_PW = os.environ["TEST_USER_PW"]
-            # sign up user
             client = boto3.client('cognito-idp', region_name=REGION)
-            response = client.sign_up(
-                ClientId=CLIENT_ID,
-                Username=TEST_USER,
-                Password=TEST_USER_PW
-            )
-            # verify user
-            response = client.admin_confirm_sign_up(
-                UserPoolId=USER_POOL_ID,
-                Username=TEST_USER
-            )
+            if user_missing(USER_POOL_ID, TEST_USER):
+                # sign up user
+                print('TEST')
+                response = client.sign_up(
+                    ClientId=CLIENT_ID,
+                    Username=TEST_USER,
+                    Password=TEST_USER_PW
+                )
+                # verify user
+                response = client.admin_confirm_sign_up(
+                    UserPoolId=USER_POOL_ID,
+                    Username=TEST_USER
+                )
     if args.action == 'destroy':
         bash_command = f'cd {path_to_infrastructure} && terraform destroy {noninteractive}'
         os.system(bash_command)
@@ -53,8 +66,5 @@ if args.component == "infrastructure":
 if args.component == "website":
     if args.action == "deploy":
         path_to_website = '../'
-        #bash_command = f'cp {path_to_infrastructure}/config.json  {path_to_website}/config.json'
-        #bash_command = f'cd {path_to_website} && npm install && npm run build'
-        # os.system(bash_command)
         bash_command = f'cd {path_to_website} && aws s3 sync build/ s3://saas-platform-frontend --acl public-read'
         os.system(bash_command)
